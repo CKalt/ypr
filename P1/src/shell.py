0b89b244461437080bf9fd65f81a1eb811731bad
JCKalt <ckaltwasser@reminingcorp.com>
2023-07-01 18:14:13 -0400

    afixed

--- P1/src/shell.py
+++ P1/src/shell.py
@@ -10,18 +10,17 @@ class Interactive:
         self.evaluator = Evaluator(verbose)
 
     def start_shell(self):
-        print("Prolog Lite - P1 Shell\nEnter your Prolog queries or use built-in commands:")
+        print("Prolog Lite - P1 Shell")
         while True:
-            command = input("Command: ")
+            command = input("> ")
+            if command == 'halt.':
+                break
             self.handle_command(command)
 
     def handle_command(self, command):
-        command_parts = command.split(' ', 1)
-        if command_parts[0] == 'load_file':
-            if len(command_parts) > 1:
-                self.load_file(command_parts[1])
-            else:
-                print("You must provide a filename.")
+        if command.startswith('consult('):
+            filename = command[len('consult('):-2]
+            self.consult(filename)
         else:
             self.evaluate_expression(command)
 
@@ -29,17 +28,17 @@ class Interactive:
         try:
             parsed_expr = self.parser.parse(expr)
             result = self.evaluator.evaluate(parsed_expr)
-            print(f"Result: {result}")
+            print(result)
         except Exception as e:
             print(f"Failed to evaluate expression. Error: {str(e)}")
 
-    def load_file(self, filename):
+    def consult(self, filename):
         try:
             with open(filename, 'r') as f:
                 for line in f:
                     self.evaluate_expression(line.strip())
         except Exception as e:
-            print(f"Failed to load file. Error: {str(e)}")
+            print(f"Failed to consult file. Error: {str(e)}")
 
 if __name__ == "__main__":
     verbose = "-v" in sys.argv
